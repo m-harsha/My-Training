e1204e53bd14dec4ecd40276b2a27165ff6ae556
m-harsha <harshachachi@gmail.com>
2022-08-30 08:50:20 +0530

    deleted write

--- FirstMaven/src/main/java/excelcode/Write.java
+++ FirstMaven/src/main/java/excelcode/Write.java
@@ -3,8 +3,7 @@ package excelcode;
 public class Write {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("hello");
+		// TODO Auto-generated method 
 	}
 
 }
